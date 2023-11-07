1e1e9595cf3e647805a00747abf58466180ef559
Patrick Eitel <patrickeitel12@gmail.com>
2023-11-06 20:48:37 -0800

    Update Item.js

---
+++ src/components/Item/Item.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Item.css"
+
+function Item({ id, name, optional, note, questName, questId, requiredLevel}) {
+  return (
+    <div className="item" onClick={(event) => console.log(event.target.id)}>
+      <p className="item-text">{name}</p>
+    </div>
+  );
+}
+
+export default Item;
