f4ac4c53c872f56bb71e936631f7e8df7523f556
Patrick Eitel <patrickeitel12@gmail.com>
2023-11-07 20:50:37 -0800

    home page now displays in a 3x3 grid

--- src/components/ClassPic/ClassPic.js
+++ src/components/ClassPic/ClassPic.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { Link } from "react-router-dom";
-import "./ClassPic.css"
+  import React from 'react';
+  import { Link } from "react-router-dom";
+  import "./ClassPic.css"
 
-function ClassPic({ id, img, setCurrentClass }) {
-  return (
-    <Link to={`/faction`}>
-    <div className="card" onClick={(event) => setCurrentClass(event.target.id)}>
-      <img className="class-image" src={img} id={id} style={{ width: '150%', height: '150%' }} alt="Class Icon" />
-    </div>
-    </Link>
-  );
-}
+  function ClassPic({ id, img, setCurrentClass }) {
+    return (
+      <Link to={`/faction`}>
+      <div className="card" onClick={(event) => setCurrentClass(event.target.id)}>
+        <img className="class-image" src={img} id={id} style={{ width: '150%', height: '150%' }} alt="Class Icon" />
+      </div>
+      </Link>
+    );
+  }
 
-export default ClassPic;
+  export default ClassPic;
