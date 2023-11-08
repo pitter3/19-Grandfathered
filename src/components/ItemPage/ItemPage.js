c555c94c8086e6e6d65a344f0523dbb218686576
Patrick Eitel <patrickeitel12@gmail.com>
2023-11-08 10:20:19 -0800

    Change conditional logic in getGear fetch call. Change logic of ItemPage.js to properly display items

--- src/components/ItemPage/ItemPage.js
+++ src/components/ItemPage/ItemPage.js
@@ -1,8 +1,22 @@
-import "./ItemPage.css"
-import Item from "../Item/Item"
+import React from 'react';
+import './ItemPage.css';
+import Item from '../Item/Item';
 
-function ItemPage({currentItems}) {
-  const allItems = currentItems.map((item) => {
+function ItemPage({ currentItems, currentFaction }) {
+  let filteredItems;
+
+  if (currentFaction === 'both') {
+    // If currentClass is 'both', don't filter; display all items.
+    filteredItems = currentItems;
+  } else {
+    // Filter items based on the selected faction.
+    filteredItems = currentItems.filter((item) => {
+      // Check if the item's faction matches the selected faction or if the item's faction is 'both'.
+      return item.faction === currentFaction || item.faction === 'both';
+    });
+  }
+
+  const allItems = filteredItems.map((item) => {
     return (
       <Item
         key={item.itemId}
@@ -15,16 +29,14 @@ function ItemPage({currentItems}) {
         requiredLevel={item.requiredLevel}
         rarity={item.rarity}
       />
-    )
-  })
-
-
+    );
+  });
 
   return (
     <div className="item-page">
       {allItems}
     </div>
-  )
+  );
 }
 
-export default ItemPage
+export default ItemPage;
