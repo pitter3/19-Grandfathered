f667f602c772a9bf369559e98e8b2d074516c32a
Patrick Eitel <patrickeitel12@gmail.com>
2023-11-09 12:33:08 -0800

    Proptypes added to Factionpage

--- src/components/FactionPage/FactionPage.js
+++ src/components/FactionPage/FactionPage.js
@@ -2,6 +2,7 @@ import "./FactionPage.css"
 import FactionPic from "../FactionPic/FactionPic"
 import ChooseYour from "../ChooseYour/ChooseYour"
 import StatusText from "../StatusText/StatusText"
+import PropTypes from "prop-types";
 
 function FactionPage({factionPics, setCurrentFaction, currentFaction, currentClass}) {
   const allFactions = factionPics.map((factionPic) => {
@@ -28,3 +29,15 @@ function FactionPage({factionPics, setCurrentFaction, currentFaction, currentCla
 }
 
 export default FactionPage
+
+FactionPage.propTypes = {
+  factionPics: PropTypes.arrayOf(
+    PropTypes.shape({
+      faction: PropTypes.string.isRequired,
+      imageUrl: PropTypes.string.isRequired,
+    })
+  ).isRequired, // factionPics is an array of objects
+  setCurrentFaction: PropTypes.func.isRequired, // setCurrentFaction is a function
+  currentFaction: PropTypes.string, // currentFaction is a string
+  currentClass: PropTypes.string, // currentClass is a string
+};
