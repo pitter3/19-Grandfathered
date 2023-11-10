4057bf3c1ee49c5dbca6dff07c3bb10f2a87290b
Patrick Eitel <patrickeitel12@gmail.com>
2023-11-09 16:50:18 -0800

    finalize the item component

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import "./Item.css"
+import "./Item.css";
 import PropTypes from "prop-types";
 
 function Item({ id, name, rarity }) {
@@ -9,17 +9,42 @@ function Item({ id, name, rarity }) {
     setChecked(!checked);
   };
 
+  const textDecorationStyle = checked ? 'line-through' : 'none';
+
   return (
     <div className="item">
-      <div className="containerx"><input type="checkbox" checked={checked} onChange={handleCheckboxChange} /><a href={`https://www.wowhead.com/wotlk/item=${id}`} className={rarity} style={{ textDecoration: checked ? 'line-through' : 'none' }}>{name}</a></div>
+      <div className="containerx">
+        <input type="checkbox" checked={checked} onChange={handleCheckboxChange} />
+        <a
+          href={`https://www.wowhead.com/wotlk/item=${id}`}
+          className={rarity}
+          style={{
+            textDecoration: textDecorationStyle,
+            cursor: 'pointer', 
+          }}
+          target="_blank" 
+          onMouseOver={(e) => {
+            if (textDecorationStyle === 'none') {
+              e.target.style.textDecoration = 'underline';
+            }
+          }}
+          onMouseOut={(e) => {
+            if (textDecorationStyle === 'none') {
+              e.target.style.textDecoration = textDecorationStyle;
+            }
+          }}
+        >
+          {name}
+        </a>
+      </div>
     </div>
   );
 }
-
-export default Item;
 
 Item.propTypes = {
   id: PropTypes.number.isRequired, 
   name: PropTypes.string.isRequired, 
   rarity: PropTypes.string.isRequired, 
 };
+
+export default Item;
